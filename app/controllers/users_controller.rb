6f50383d39f4c0bca6393135e75beb98cce3d6bb
dani <danifromecuador@gmail.com>
2024-10-29 16:18:07 -0500

    Create User controller kfeat/services-controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UserController < ApplicationController
+  # GET /users
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user, status: :unprocessable_entity
+    end
+  end
+end
