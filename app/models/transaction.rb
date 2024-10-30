a530926acc5fc9b460a0f870c86f797e1ba3287b
dani <danifromecuador@gmail.com>
2024-10-30 15:17:58 -0500

    Add Transaction validation to avoid transaction if insuficient balance feat/validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,6 +5,7 @@ class Transaction < ApplicationRecord
             :amount_to_receive_in_cents_or_sats, :btc_usd_exchange_rate, presence: true
   # Validaciones personalizadas
   validate :minimal_amount_to_send
+  validate :avoid_transaction_if_insuficient_balance
 
   private
 
@@ -17,5 +18,15 @@ class Transaction < ApplicationRecord
 
     errors.add(:minimal_amount,
                'La cantidad mínima a enviar debe ser de 14 000 sats o superior')
+  end
+
+  def avoid_transaction_if_insuficient_balance
+    @user = User.find(user_id)
+    if coin_to_send == 'usd' && @user.usd_balance_in_cents < amount_to_send_in_cents_or_sats
+      errors.add(:insuficient_balance, 'No tiene fondos en USD suficientes para realizar esta transacción')
+    end
+    return unless coin_to_send == 'btc' && @user.btc_balance_in_sats < amount_to_send_in_cents_or_sats
+
+    errors.add(:insuficient_balance, 'No tiene fondos suficientes en BTC para realizar esta transacción')
   end
 end
