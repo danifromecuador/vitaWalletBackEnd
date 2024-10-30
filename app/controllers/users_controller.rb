709d1a41315ea5090a2eed4baadbe33e21512f1f
dani <danifromecuador@gmail.com>
2024-10-29 19:46:53 -0500

    Create aditional CRUD methods for Users controller feat/services-controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show update destroy ]
+
   # GET /users
   def index
     @users = User.all
@@ -18,5 +20,28 @@ class UserController < ApplicationController
     else
       render json: @user, status: :unprocessable_entity
     end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @group.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :year_of_birth, :usd_balance_in_cents, :btc_balance_in_sats)
   end
 end
