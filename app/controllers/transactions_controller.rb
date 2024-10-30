dbf5486915a6b326a78ecb2df61152cf4ec121cc
dani <danifromecuador@gmail.com>
2024-10-30 02:50:16 -0500

    Fix rubocop errors feat/services-controllers

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,34 +1,34 @@
 class TransactionsController < ApplicationController
-  # before_action :set_user
+  before_action :set_user
 
   # GET /users/1/transactions
   def index
-    @user = User.find(params[:user_id])
     @transactions = @user.transactions
     render json: @transactions
   end
 
   # GET /users/1/transactions/1
   def show
-    @user = User.find(params[:user_id])
     @transaction = @user.transactions.find(params[:id])
     render json: @transaction
   end
 
   # POST /users/1/transactions
   def create
-    user = User.find(params[:user_id])
-    transaction = user.transactions.build(
+    @current_btc_price = CoinDeskService.current_btc_price
+    @transaction = @user.transactions.build(
       coin_to_send: transaction_params[:coin_to_send],
       coin_to_receive: transaction_params[:coin_to_receive],
       amount_to_send_in_cents_or_sats: transaction_params[:amount_to_send_in_cents_or_sats],
-      amount_to_receive_in_cents_or_sats: 12345678,
-      btc_usd_exchange_rate: 65234.2356
+      amount_to_receive_in_cents_or_sats: calculate_received_amount(
+        transaction_params[:coin_to_send], transaction_params[:amount_to_send_in_cents_or_sats], @current_btc_price
+      ),
+      btc_usd_exchange_rate: @current_btc_price
     )
-    if transaction.save
-      render json: transaction, status: :created
+    if @transaction.save
+      render json: @transaction, status: :created
     else
-      render json: transaction.errors, status: :unprocessable_entity
+      render json: @transaction.errors, status: :unprocessable_entity
     end
   end
 
@@ -36,5 +36,16 @@ class TransactionsController < ApplicationController
 
   def transaction_params
     params.require(:transaction).permit(:coin_to_send, :coin_to_receive, :amount_to_send_in_cents_or_sats)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  # Calculate received amount in cents or sats
+  def calculate_received_amount(coin_to_send, amount_to_send, exchange_rate)
+    return (amount_to_send / exchange_rate) * 1_000_000 if coin_to_send == 'usd'
+
+    (amount_to_send * exchange_rate) / 1_000_000
   end
 end
