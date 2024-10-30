60630c542741b447ca84fd4cb7985c1dd2b85e8c
dani <danifromecuador@gmail.com>
2024-10-30 14:20:59 -0500

    Add User validations for name, legal age, and balance feat/validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_many :transactions
+  # Validaciones de atributos
+  validates :name, :year_of_birth, :usd_balance_in_cents, :btc_balance_in_sats, presence: true
+  validates :usd_balance_in_cents, :btc_balance_in_sats, numericality: { greater_than_or_equal_to: 0 }
+  # Validaciones personalizadas
+  validate :must_be_of_legal_age
+
+  private
+
+  def must_be_of_legal_age
+    return unless year_of_birth.present? && Date.current.year - year_of_birth < 18
+
+    errors.add(:year_of_birth, 'El usuario debe ser mayor de edad para registrase')
+  end
 end
