a4a08a5debed57e97e49d2791e0fc46168aa56aa
dani <danifromecuador@gmail.com>
2024-10-30 13:38:28 -0500

    Allow the Users controller to update for changing balance feat/services-controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,16 @@ class UsersController < ApplicationController
     end
   end
 
+  # PATCH/PUT /users/1
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
