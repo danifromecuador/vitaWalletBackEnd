c4c46df0e0d1a321654fc03aab6e4d74329b1c47
dani <danifromecuador@gmail.com>
2024-10-30 14:46:49 -0500

    Add Transaction validatios for presence and minimal amount to send feat/validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,21 @@
 class Transaction < ApplicationRecord
   belongs_to :user
+  # Validaciones de atributos
+  validates :user_id, :coin_to_send, :coin_to_receive, :amount_to_send_in_cents_or_sats,
+            :amount_to_receive_in_cents_or_sats, :btc_usd_exchange_rate, presence: true
+  # Validaciones personalizadas
+  validate :minimal_amount_to_send
+
+  private
+
+  def minimal_amount_to_send
+    if coin_to_send == 'usd' && amount_to_send_in_cents_or_sats < 1000
+      errors.add(:minimal_amount,
+                 'La cantidad mínima a enviar debe ser de 1 000 cents o superior')
+    end
+    return unless coin_to_send == 'btc' && amount_to_send_in_cents_or_sats < 14_000
+
+    errors.add(:minimal_amount,
+               'La cantidad mínima a enviar debe ser de 14 000 sats o superior')
+  end
 end
